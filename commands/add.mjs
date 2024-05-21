b1816ab335318c42a166cc07e40894c63c7071d8
Adam Brodzinski <adambrodzinski@gmail.com>
2024-05-20 22:38:05 -0500

    wip: add pt1

--- commands/add.mjs
+++ commands/add.mjs
@@ -1,16 +1,29 @@
 import { spawn } from "child_process";
 import { isDuneMissing } from "../utils.mjs";
+import { parseConfig, writeConfig } from "../utils.mjs";
 
-const COMMAND = "add <package-names...>";
+const COMMAND = "add <package> <version>";
 const DESCRIPTION = "Add packages to your project, optionally as dev dependencies";
 
-async function handler(names, _options, _command) {
+async function handler(packageName, version, options) {
   if (await isDuneMissing()) return;
+  console.log({ packageName, version });
+  console.log(`Dev: ${options.dev ? "Yes" : "No"}`);
 
   const hasDoubleDash = process.argv.includes("--");
-  const duneArgs = hasDoubleDash ? process.argv.slice(process.argv.indexOf("--") + 1) : [];
+  const opamArgs = hasDoubleDash ? process.argv.slice(process.argv.indexOf("--") + 1) : [];
 
-  const utop = spawn("opam", ["install", ...names, ...duneArgs], { stdio: "inherit" });
+  const configJson = parseConfig();
+  console.log("CONFIG", configJson);
+
+  if (configJson.dependencies === undefined) {
+    configJson.dependencies = {};
+  }
+  configJson.dependencies[packageName] = version;
+
+  writeConfig(configJson);
+
+  const utop = spawn("opam", ["install", packageName, ...opamArgs], { stdio: "inherit" });
 
   utop.on("error", (error) => {
     console.log("Failed to start subprocess.");
--- utils.mjs
+++ utils.mjs
@@ -1,12 +1,14 @@
+import fs from "fs";
 import { exec } from "child_process";
+import TOML from "smol-toml";
+
+const CONFIG_FILENAME = "mycaml.toml";
 
 const DUNE_MISSING_MSG =
   "dune is required and is not installed. Please run `opam update; opam install dune` and try again";
 
 export function isCommandMissing(command, msg) {
-  let errMessage = msg
-    ? msg
-    : "Error, command ${command} is not available on the path";
+  let errMessage = msg ? msg : "Error, command ${command} is not available on the path";
 
   return new Promise((resolve) => {
     exec(`which ${command}`, (error, _stdout, stderr) => {
@@ -22,4 +24,27 @@ export function isCommandMissing(command, msg) {
 
 export function isDuneMissing() {
   return isCommandMissing("dune", DUNE_MISSING_MSG);
+}
+
+export function parseConfig() {
+  try {
+    const config = fs.readFileSync(findConfigPath(), "utf8");
+    return TOML.parse(config);
+  } catch (err) {
+    if (err?.code === "ENOENT") {
+      console.log("Could not find a mycaml.toml file in this directory.");
+      return;
+    }
+    throw err;
+  }
+}
+
+export function writeConfig(configJson) {
+  // TODO: check for file before writing new one
+  const tomlStr = TOML.stringify(configJson);
+  fs.writeFileSync(findConfigPath(), tomlStr);
+}
+
+function findConfigPath() {
+  return `${process.cwd()}/${CONFIG_FILENAME}`;
 }
