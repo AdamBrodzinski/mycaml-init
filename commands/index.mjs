1ba6ee0eadfe7020d56b8579b8ecfc5d13e90933
Adam Brodzinski <adambrodzinski@gmail.com>
2024-05-24 09:54:42 -0500

    wip: new project

--- commands/index.mjs
+++ commands/index.mjs
@@ -1,5 +1,6 @@
 export * from "./add.mjs";
+export * from "./build.mjs";
+export * from "./new.mjs";
 export * from "./remove.mjs";
 export * from "./run.mjs";
-export * from "./build.mjs";
 export * from "./shell.mjs";
